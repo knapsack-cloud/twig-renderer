0382c4f6f3340a5b00641d0f8bf4c92313916a4f
Evan Lovely <EvanLovely@users.noreply.github.com>
2018-07-03 17:51:49 -0700

    lint fix

--- src/index.js
+++ src/index.js
@@ -43,8 +43,6 @@ class TwigRenderer {
   }
 
   async init() {
-    await fs.writeFile(sharedConfigPath, JSON.stringify(this.config, null, '  '));
-    
     // @todo improve method of selecting a port to try
     // Just because a port is available now, doesn't mean it wont be taken in 5ms :P
     const portAttempt = getRandomInt(10000, 65000);
@@ -52,6 +50,8 @@ class TwigRenderer {
     this.settings.phpServerUrl = `127.0.0.1:${port}`;
 
     const sharedConfigPath = path.join(__dirname, `shared-config--${port}.json`);
+    await fs.writeFile(sharedConfigPath, JSON.stringify(this.config, null, '  '));
+
     // @todo Pass config to PHP server a better way than writing JSON file, then reading in PHP
 
     this.phpServer = execa('php', [
@@ -63,10 +63,10 @@ class TwigRenderer {
     // @todo wrap this in config for seeing it besides `verbose` - too noisy
     this.phpServer.stdout.pipe(process.stdout);
     this.phpServer.stderr.pipe(process.stderr);
-    
+
     // @todo detect when PHP server is ready to go; in meantime, we'll just pause for a moment
     await sleep(3000);
-    
+
     if (this.config.verbose) {
       console.log(`TwigRender js init complete. PHP server started on port ${port}`);
     }
