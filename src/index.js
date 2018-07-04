1f613d796bc17782ad9bfba07447b0121db4b2b6
Evan Lovely <EvanLovely@users.noreply.github.com>
2018-07-03 17:39:25 -0700

    cleanup

--- src/index.js
+++ src/index.js
@@ -43,7 +43,8 @@ class TwigRenderer {
   }
 
   async init() {
-    await sleep(1000);
+    await fs.writeFile(sharedConfigPath, JSON.stringify(this.config, null, '  '));
+    
     // @todo improve method of selecting a port to try
     // Just because a port is available now, doesn't mean it wont be taken in 5ms :P
     const portAttempt = getRandomInt(10000, 65000);
@@ -52,7 +53,6 @@ class TwigRenderer {
 
     const sharedConfigPath = path.join(__dirname, `shared-config--${port}.json`);
     // @todo Pass config to PHP server a better way than writing JSON file, then reading in PHP
-    await fs.writeFile(sharedConfigPath, JSON.stringify(this.config, null, '  '));
 
     this.phpServer = execa('php', [
       '-S',
@@ -60,15 +60,16 @@ class TwigRenderer {
       path.join(__dirname, 'server.php'),
     ]);
 
+    // @todo wrap this in config for seeing it besides `verbose` - too noisy
     this.phpServer.stdout.pipe(process.stdout);
     this.phpServer.stderr.pipe(process.stderr);
-
+    
+    // @todo detect when PHP server is ready to go; in meantime, we'll just pause for a moment
+    await sleep(3000);
+    
     if (this.config.verbose) {
       console.log(`TwigRender js init complete. PHP server started on port ${port}`);
     }
-
-    // @todo detect when PHP server is ready to go; in meantime, we'll just pause for a moment
-    await sleep(1000);
     return true;
   }
 
