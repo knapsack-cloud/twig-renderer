51d1280e9613d8fb94a5a3f50c0827c34509ffb3
Evan Lovely <evanlovely@gmail.com>
2018-07-05 18:04:47 -0700

    Implementing async PHP server that keeps Twig Env in-memory. Passes 1000 template test.

---
+++ __tests__/volume/volume.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const fs = require('fs-extra');
+
+const TwigRenderer = require('../../src');
+
+describe('volume', () => {
+  const twigRenderer = new TwigRenderer({
+    src: {
+      roots: [
+        path.join(__dirname, 'src'),
+      ],
+    },
+    autoescape: false,
+    verbose: false,
+  });
+
+  test('volume - 1000 basic renders', async () => {
+    await fs.emptyDir(path.join(__dirname, 'dist'));
+
+    const renders = [];
+
+    for (let i = 0; i < 1000; i++) {
+      renders.push(new Promise(async (resolve) => {
+        const result = await twigRenderer.render(`item-${i}.twig`);
+
+        if (result.ok) {
+          await fs.writeFile(path.join(__dirname, 'dist', `item-${i}.html`), result.html);
+        } else {
+          // console.error('Error: ', result.message);
+          // reject(result);
+        }
+        resolve(result);
+      }));
+    }
+
+    const results = await Promise.all(renders);
+
+    expect(results).toMatchSnapshot();
+    expect(results.filter(result => !result.ok)).toEqual([]);
+  }, 15000);
+
+  afterAll(() => twigRenderer.closeServer());
+});
