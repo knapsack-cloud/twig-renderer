a7763b8327c405e2139aa06d68f9cb8f0ea76e8d
Evan Lovely <EvanLovely@users.noreply.github.com>
2023-11-06 13:54:36 -0800

    setup better npm publishing

---
+++ auto.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  onlyPublishWithReleaseLabel: false,
+  prereleaseBranches: [],
+  baseBranch: 'master',
+  author: 'KnapsackBot <53622700+KnapsackBot@users.noreply.github.com>',
+  plugins: [
+    [
+      // https://intuit.github.io/auto/docs/generated/npm
+      'npm',
+    ],
+    // https://intuit.github.io/auto/docs/generated/released
+    'released',
+  ],
+};
